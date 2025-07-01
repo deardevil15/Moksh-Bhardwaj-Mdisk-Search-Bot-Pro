3a630a4b4afb70dada97e027db87c9cc9395709e
deardevil15 <157885783+deardevil15@users.noreply.github.com>
2025-07-01 12:48:02 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,12 @@
-echo "Cloning Repo...."
+#!/bin/bash
+
+echo "Cloning Repo..."
 git clone https://github.com/Greymattersbot/Link-Search-Bot-Pro /MdiskWalaBot
-cd /MdiskWalaBot
+
+cd /MdiskWalaBot || exit 1
+
+echo "Installing requirements..."
 pip3 install -r requirements.txt
-echo "Starting Bot...."
+
+echo "Starting Bot..."
 python3 main.py
